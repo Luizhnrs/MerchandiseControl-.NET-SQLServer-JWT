0833af79ed7405abbab3d0d948d497ef30f7a42b
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-23 16:09:09 -0300

    DpInjection

--- MerchandiseControl/Program.cs
+++ MerchandiseControl/Program.cs
@@ -1,9 +1,13 @@
+using MerchandiseControl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IMerchandiseRepository, MerchandiseRepository>();
 
 var app = builder.Build();
 
