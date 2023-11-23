2e8fdb3d2678a1e94250db5f26eb51cc0f3ba5ed
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-23 16:02:30 -0300

    StringConnectionImp

--- MerchandiseControl/Infrastructure/ConnectionContext.cs
+++ MerchandiseControl/Infrastructure/ConnectionContext.cs
@@ -5,4 +5,6 @@ namespace MerchandiseControl;
 public class ConnectionContext : DbContext
 {
     public DbSet <Merchandise> Merchandises {get; set;}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS; Initial Catalog= MerchandiseControl; TrustServerCertificate=True; Integrated Security=true; ");
 }
---
+++ MerchandiseControl/Infrastructure/MerchandiseRepository.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MerchandiseControl;
+
+public class MerchandiseRepository : IMerchandiseRepository
+{
+    private readonly ConnectionContext _context = new ConnectionContext();
+    public void add(Merchandise merchandise)
+    {
+        _context.Merchandises.Add(merchandise);
+        _context.SaveChanges();
+    }
+
+    public List<Merchandise> get()
+    {
+        throw new NotImplementedException();
+    }
+}
