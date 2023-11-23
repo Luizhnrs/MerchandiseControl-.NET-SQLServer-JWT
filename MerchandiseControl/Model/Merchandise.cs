e65b387cf3fb825be0a7a816aad6e08fcb02be85
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-23 15:36:22 -0300

    ModelContructor

--- MerchandiseControl/Model/Merchandise.cs
+++ MerchandiseControl/Model/Merchandise.cs
@@ -1,6 +1,22 @@
-﻿namespace MerchandiseControl;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
 
+namespace MerchandiseControl;
+[Table("MerchandiseControl")]
 public class Merchandise
 {
+    [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string expirationDate { get; set; }
+        public float price { get; set; }
 
+        public Merchandise(string name, string expirationDate, float price) 
+        {
+            this.name = name ?? throw new ArgumentNullException (nameof(name));
+            this.expirationDate = expirationDate;
+            this.price = price;
+        
+        }
 }
